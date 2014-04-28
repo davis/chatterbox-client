34862129412095694eca998c8e77655d31c478cc
Hack Reactor Students <dchk90@gmail.com>
2014-04-28 16:35:59 -0700

    Fetches most recent messages sent to server

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,12 +1,26 @@
+// make linter happy
+var $ = $;
+var _ = _;
+var moment = moment;
+// end
+
 // YOUR CODE HERE:
 
 
 var app = {
   init : function() {
-    this.server = 'something'
+    this.server = 'something';
   },
 
+  lastRefresh: 0,
+
   send: function(message) {
+    message = message || {
+      'username': 'davis',
+      'text': 'hats on cats',
+      'roomname': 'lobby'
+    };
+
     $.ajax({
       // always use this url
       url: 'https://api.parse.com/1/classes/chatterbox',
@@ -15,6 +29,11 @@ var app = {
       contentType: 'application/json',
       success: function (data) {
         console.log('chatterbox: Message sent');
+        console.dir(data);
+        console.log(JSON.stringify(message));
+      },
+      complete: function (data) {
+        console.log('complete')
       },
       error: function (data) {
         // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
@@ -24,24 +43,34 @@ var app = {
   },
   fetch: function() {
     $.ajax({
-      url: 'https://api.parse.com/1/classes/chatterbox',
+      url: 'https://api.parse.com/1/classes/chatterbox?order=-createdAt',
       success: function (data) {
         console.log('chatterbox: Successfully fetched messages');
+        console.log(data);
         app.render(data);
+        app.lastRefresh = Date.now();
       },
       error: function (data) {
         console.error('chatterbox: Failed to fetch messages');
       }
     });
   },
+
   render: function(data) {
-    console.log(data);
-    _.each(data.results, function(messageObj) {
-      $('body').append(app.template(messageObj.username, messageObj.text));
-    });
+    for(var i = data.results.length-1; i >= 0; i--){
+      var m = data.results[i];
+      var date = new Date(m.createdAt);
+      if (date > app.lastRefresh){
+        console.log(m.username, m.text);
+        $('.messages').prepend(app.template(m));
+      }
+    }
   },
 
-  template: function(username, text) {
-    return '<div>' + username + ': ' + text + '</div>';
+  template: function(m) {
+    var $message = $('<div class="message"></div>');
+    var date = moment(m.createdAt).fromNow();
+    $message.text('(' + date + ')' + m.username + ': ' + m.text + ' (' + m.objectId + ')');
+    return $message;
   }
 };
