5098481c487876e9ecfdac1bd87220957533b2c3
Hack Reactor Students <dchk90@gmail.com>
2014-04-29 10:54:27 -0700

    includes templating for messages

--- client/scripts/message.js
+++ client/scripts/message.js
@@ -1,7 +1,27 @@
-var Message = function(text, createdAt, objectId) {
+var _ = _;
+var moment = moment;
+
+var Message = function(text, createdAt, objectId, username, chatroom) {
   this._text = text;
   this._createdAt = createdAt;
   this._objectId = objectId;
+  this._username = username;
+  this._chatroom = chatroom;
+  this._timeAsString = this.timeToString();
 };
 
-Message.prototype.hello = function(){};
+Message.prototype.hello = function(){
+  alert('hi');
+};
+
+Message.prototype.timeToString = function() {
+  return moment(this._createdAt).fromNow();
+};
+
+Message.prototype.templated = _.template('<div class="message"> \
+                                            <div class="message-header"> \
+                                              <a class="username" href="#"><%- this._username %></a><small class="created-at"><%- this._timeAsString %></small> \
+                                            </div> \
+                                            <p class="message-text"><%- this._text %></p> \
+                                          </div>');
+
