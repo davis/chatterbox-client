65203c7a43029edcd4ce3df18b4174ff3b581c1a
Hack Reactor Students <dchk90@gmail.com>
2014-04-28 17:28:34 -0700

    Adds ability to send messages

--- client/scripts/interact.js
+++ client/scripts/interact.js
@@ -6,7 +6,36 @@ var app = app;
 
 
 $(document).ready(function() {
+  var send = function() {
+    var $text = $('.composeMessage').val();
+    var username = window.location.search.split('username=')[1].split('&')[0];
+    var message = {
+      username: username,
+      text: $text,
+      roomname: 'lobby'
+    };
+    app.send(message);
+    $('.composeMessage').val('');
+  };
+
   $('.fetch').click(function() {
     app.fetch();
   });
+
+  $('.sendMessage').click(function() {
+    send();
+  });
+
+  $('.composeMessage').keyup(function(e) {
+    if (e.which === 13) {
+      send();
+    }
+  });
+
+  setInterval(function() {
+    app.fetch();
+  }, 2000);
+
+
+
 });
