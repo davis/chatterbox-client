756e68a1a3d868faa2cd5c34763a803eb0038612
Hack Reactor Students <dchk90@gmail.com>
2014-04-29 20:09:03 -0700

    splits interactive functionality out into modules

--- client/scripts/interact.js
+++ client/scripts/interact.js
@@ -1,61 +1,20 @@
 // make linter happy
 var $ = $;
 var app = app;
-
 // end
 
+// interact with messageList
 
-$(document).ready(function() {
-  var send = function() {
-    var $text = $('.composeMessage').val();
-    var username = window.location.search.split('username=')[1].split('&')[0];
-    var message = {
-      username: username,
-      text: $text,
-      roomname: app.activeChatRoom || 'lobby'
-    };
-    app.send(message);
-    $('.composeMessage').val('');
-  };
-
-  $('.fetch').click(function() {
-    app.fetch();
-  });
-
-  $('.sendMessage').click(function() {
-    send();
-  });
-
-  $('.composeMessage').keyup(function(e) {
-    if (e.which === 13) {
-      send();
-    }
-  });
-
-  $('body').on('click', '.username', function() {
-    var newFriend = $(this).text();
-    if (!app.friends[newFriend]) { // if friend is not in friends array
-      app.friends[newFriend] = true;
-      app.render();
-    }
-  });
-
-  $('body').on('click', '.chatroom', function() {
-    app.activeChatRoom = $(this).text();
-    $(this).addClass('bold');
-    app.render();
-  });
-
-  $('.title').click(function(){
-    app.activeChatRoom = undefined;
-    app.render();
-    app.renderChatRoom();
+$(function(){
+  // Toggle friendship of a user when username is clicked.
+  $('body').on('click', '.username', function(event) {
+    event.preventDefault();
+    app.allUsers[$(this).text()] = !app.allUsers[$(this).text()];
+    app.renderRoom();
   });
 
   setInterval(function() {
     app.fetch();
   }, 2000);
-
-
 
 });
