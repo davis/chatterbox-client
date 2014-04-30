39bd45f1281c6b9c627490b984757d70d89333c7
Hack Reactor Students <dchk90@gmail.com>
2014-04-29 20:05:45 -0700

    refactors app to be more modular

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -9,23 +9,26 @@ var app = {
 
   // initializes messages, chatrooms, and users to blank arrays/objects
   init: function() {
-    this.allMessages = {};
-    this.allChatRooms = {};
-    this.allUsers = {};
+    app.allMessages = {};
+    app.allChatRooms = {};
+    app.activeChatRoom = 'lobby';
+    app.allUsers = {};
+    app.server = 'https://api.parse.com/1/classes/chatterbox';
+    app.fetch();
   },
 
   // fetch should pull last 100 msgs from server
   fetch: function() {
     $.ajax({
-      url: 'https://api.parse.com/1/classes/chatterbox',
+      url: app.server,
       data: {
         order: '-createdAt'
       },
       success: function (data) {
         console.log('chatterbox: Successfully fetched messages');
-        app.organize.call(app, data.results);
+        app.organize(data.results);
       },
-      error: function (data) {
+      error: function () {
         console.error('chatterbox: Failed to fetch messages');
       }
     });
@@ -34,75 +37,91 @@ var app = {
   // organize takes the messages array fetch returns and loads it into the users and chatrooms
   organize: function(messages) {
     // for each msg
-    for(var i = 0; i < messages.length; i++) {
+
+    for(var i = messages.length-1; i >= 0; i--) {
       var m = messages[i];
       // check if obj id is in all messages
-      if(!this.allMessages[m.objectId]) {
-        // if we don't have the user, create new user
-        if(!this.allUsers[m.username]) {
-          this.allUsers[m.username] = 1;
-        } else {
-          this.allUsers[m.username]++;
+      if(!app.allMessages[m.objectId]) {
+        // if we don't have the user or they are not friends, create/set new user to not a friend
+        if(!app.allUsers[m.username]) {
+          app.allUsers[m.username] = false;
         }
         // if we don't have the chatroom, create new chatroom
-        if(!this.allChatRooms[m.roomname]) {
-          this.allChatRooms[m.roomname] = 1;
-        } else {
-          this.allChatRooms[m.roomname]++;
+        if(app.allChatRooms[m.roomname] === undefined) { // revisit making chatroom into a class and checking
+          app.allChatRooms[m.roomname] = true;
+          app.renderChatRoomList(m.roomname);
         }
         // push to both user and chatroom
         var message = new Message(m.text, m.createdAt, m.objectId, m.username, m.roomname);
-        this.allMessages[m.objectId] = true;
-        app.render(message);
+        app.allMessages[m.objectId] = message;
+        app.renderMessage(message);
       }
     }
   },
 
-  render: function(message) {
-    $('.message-list').prepend($(message.templated()));
+  renderMessage: function(message) {
+    $('.message-list').prepend($(message.templated())); // prepend to dom
+    app.renderRoom();
+    // if (message.chatroom === app.activeChatRoom) {
+    //   $('#'+message.objectId).removeClass('hidden');
+    // // else (unless no room is selected) add hidden class
+    // } else if (app.activeChatRoom !== null) {
+    //   $('#'+message.objectId).addClass('hidden');
+    // }
   },
 
-  renderChatRooms: function() {
-    $('#chatroomlist').html('');
-    for(var chatroom in app.chatRooms) {
-      var $chatroom = $('<li><a href="#" class="chatroom">' + chatroom + '</a></li>');
-      $('#chatroomlist').append($chatroom);
+// Fix when no room is selected, show everything
+  renderRoom: function(){
+    for(var key in app.allMessages) {
+      var needsRendering = false;
+      var m = app.allMessages[key];
+      if(m.timeAsString !== m.timeToString()) { // checks if message needs to be updated based on time
+        m.timeAsString = m.timeToString();
+        needsRendering = true;
+      }
+      if(m.fromFriend !== m.isFromAFriend()) { // checks if message needs to be bolded (for friend)
+        m.fromFriend = m.isFromAFriend();
+        needsRendering = true;
+      }
+      // if chatroom our message is in is the active chatroom, remove hidden class
+      if (m.chatroom === app.activeChatRoom) {
+        $('#'+key).removeClass('hidden');
+      // else (unless no room is selected) add hidden class
+      } else if (app.activeChatRoom !== null) {
+        $('#'+key).addClass('hidden');
+      }
+      if(needsRendering) {
+        $('#'+key).html(m.updatedTemplate());
+      }
     }
+  },
+
+  renderChatRoomList: function(roomname) {
+
+    $('.dropdown-menu').append('<li><a class="chatroom-link">' + roomname + '</a></li>'); //template needs to go into a class
+
   },
 
   send: function(message) {
     $.ajax({
-      // always use this url
-      url: 'https://api.parse.com/1/classes/chatterbox',
+      // always use app url
+      url: app.server,
       type: 'POST',
       data: JSON.stringify(message),
       contentType: 'application/json',
-      success: function (data) {
+      success: function () {
         console.log('chatterbox: Message sent');
       },
-      complete: function (data) {
-        console.log('complete');
-      },
-      error: function (data) {
+      error: function () {
         // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
         console.error('chatterbox: Failed to send message');
+      },
+      complete: function () {
+        console.log('complete');
       }
     });
   },
-
-  template: function(m, isFriend) {
-    if(isFriend) {
-      var $message = $('<div class="bold"></div>');
-    } else {
-      var $message = $('<div class="message"></div>');
-    }
-    var $user = $('<a href ="#" class="username"></a>');
-    $user.text(m.username);
-    var date = moment(m.createdAt).fromNow();
-    $message.text(': ' + m.text + '(' + date + ')' + ' in ' + m.roomname);
-    return $message.prepend($user);
-  }
 };
 
-
 app.init();
+
